ebd354aac25779b2a89414ac52753e7e19a627db
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-11-04 13:06:49 +0600

    created login and signup pages

---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl text-center">Welcome Login Page!</h1>
+    </main>
+  );
+}
---
+++ app/signup/page.jsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl text-center">Welcome Sign Up Page!</h1>
+    </main>
+  );
+}
