f6467d3320ea94bd997fb93ada0bc9ccf676ba9f
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-11-04 13:01:17 +0600

    working on create folder basic folder structure

---
+++ app/me/page.jsx
@@ -0,0 +1,9 @@
+export default function CustomerHome() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl text-center">
+        Welcome to Customer Home Page!
+      </h1>
+    </main>
+  );
+}
---
+++ app/we/page.jsx
@@ -0,0 +1,9 @@
+export default function OwnerHome() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl text-center">
+        Welcome Owner Home Page!
+      </h1>
+    </main>
+  );
+}
