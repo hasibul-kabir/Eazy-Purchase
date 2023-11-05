2de80593318b8596a122354796c908be8776bf5e
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-11-05 21:13:17 +0600

    fixed product filter issue

--- app/products/actions/filterProducts.js
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-
-export default async function filterProducts(filters) {
-  const params = new URLSearchParams([["category", filters._id]]);
-
-  redirect(`/products?${params}`);
-}
--- app/products/components/Category.jsx
+++ app/products/components/Category.jsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { useState } from "react";
-import filterProducts from "../actions/filterProducts";
+import { useRouter } from "next/navigation";
 
 export default function Category({ category }) {
-  const handleFilter = (selectedCategory) => {
-    filterProducts(selectedCategory);
-  };
+  const router = useRouter();
 
   return (
     <div
       key={category.id}
-      onClick={handleFilter.bind(null, category)}
+      onClick={() => {
+        router.push(`/products?category=${category._id}`);
+      }}
       className="bg-secondary text-main px-3 py-2 rounded-md cursor-pointer"
     >
       {category.name}
